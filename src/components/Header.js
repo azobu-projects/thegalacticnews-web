dccec5a06b2881fe85c13b709cc9795c37059b80
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-04 22:08:39 +0700

    Style header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid #cccccc;
+`
+
+const HeaderSegment = styled.div`
+  padding: 20px;
+`
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <HeaderStyled>
+      <HeaderSegment>
         <a href='/menu'>Menu</a>
-      </div>
-
-      <h1>
-        <img src='/assets/images/logo.svg' alt='The Galactic News' />
-      </h1>
-
-      <div>
+      </HeaderSegment>
+      <HeaderSegment>
+        <a href='/'>
+          <img src='/assets/images/logo.svg' alt='The Galactic News' />
+        </a>
+      </HeaderSegment>
+      <HeaderSegment>
         <a href='/register'>Register</a>
         <a href='/login'>Login</a>
-      </div>
-    </header>
+      </HeaderSegment>
+    </HeaderStyled>
   )
 }
 
