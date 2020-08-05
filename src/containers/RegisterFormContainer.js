85a5024eb84ee72873307334732c5d08e4df6038
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-05 23:09:07 +0700

    Style register form

--- src/containers/RegisterFormContainer.js
+++ src/containers/RegisterFormContainer.js
@@ -1,5 +1,44 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import styled, { useColorMode } from '@xstyled/emotion'
+
+const Form = styled.form``
+
+const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20;
+`
+
+const Label = styled.label`
+  font-weight: bold;
+  font-size: 1.2em;
+`
+
+const Input = styled.input`
+  padding: 3;
+  font-size: 1.2em;
+  border: 2;
+  border-radius: 5;
+  border-color: border;
+`
+
+const Submit = styled.input`
+  cursor: pointer;
+  border-radius: 5;
+  border: none;
+  color: #fff;
+  font-size: 1.2em;
+  font-weight: bold;
+  letter-spacing: 0.1em;
+  padding: 3;
+  text-transform: uppercase;
+  width: 100%;
+  background-color: primary;
+  &:hover {
+    background-color: secondary;
+  }
+`
 
 const RegisterFormContainer = () => {
   const { register, handleSubmit, errors } = useForm()
@@ -9,17 +48,31 @@ const RegisterFormContainer = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name='email' type='text' ref={register({ required: true })} />
-      {errors.email && <span>Valid email is required</span>}
-      <input
-        name='password'
-        type='password'
-        ref={register({ required: true })}
-      />
-      {errors.password && <span>Password is required</span>}
-      <input type='submit' value='Register' />
-    </form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Field>
+        <Label htmlFor='email'>Email Address</Label>
+        <Input
+          name='email'
+          type='text'
+          ref={register({ required: true })}
+          placeholder='name@planet.com'
+        />
+        {errors.email && <span>Valid email is required</span>}
+      </Field>
+
+      <Field>
+        <Label htmlFor='password'>Password</Label>
+        <Input
+          name='password'
+          type='password'
+          ref={register({ required: true })}
+          placeholder='*****'
+        />
+        {errors.password && <span>Password is required</span>}
+      </Field>
+
+      <Submit type='submit' value='Create Account' />
+    </Form>
   )
 }
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from '@xstyled/emotion'
 
 import { Page, Hero } from '../components'
 import { RegisterFormContainer } from '../containers'
+
+const Row = styled.div`
+  max-width: 480;
+  margin: 0 auto;
+`
 
 const Register = () => {
   return (
     <Page>
       <Hero heading='Create Your Account' />
-      <RegisterFormContainer />
+      <Row>
+        <RegisterFormContainer />
+      </Row>
     </Page>
   )
 }
